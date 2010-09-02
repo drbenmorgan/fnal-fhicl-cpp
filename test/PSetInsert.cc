d1cec1535707943b7628e0f76c6db7ca9470ac55
Qiming Lu <qlu@oink.fnal.gov>
2010-09-01 22:04:59 -0500

    Added PSetInsert.cc test file (left over in the last commit)

---
+++ test/PSetInsert.cc
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "ParameterSet.h"
+
+int main()
+{
+  mf::ParameterSet pset;
+
+  pset.addInt("a", 1);
+  pset.addString("b", "string");
+  pset.addDouble("c", 2.3);
+  pset.addBool("d", true);
+
+  std::vector<int> vi;
+  vi.push_back(1);
+  vi.push_back(2);
+  vi.push_back(3);
+  pset.addVInt("e", vi);
+
+  std::vector<double> vd;
+  vd.push_back(0.1);
+  vd.push_back(0.2);
+  vd.push_back(0.3);
+  pset.addVDouble("f", vd);
+
+  std::vector<std::string> vs;
+  vs.push_back("str1");
+  vs.push_back("str2");
+  vs.push_back("str3");
+  pset.addVString("g", vs);
+
+  mf::ParameterSet pset2;
+  pset2.addInt("a2", 1);
+  pset2.addString("b2", "bstring");
+  pset.addPSet("h", pset2);
+
+  pset.print();
+
+  return 0;
+}

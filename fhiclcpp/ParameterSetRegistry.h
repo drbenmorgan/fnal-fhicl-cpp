7f352ae0708b1541e01feadbd745cbaa22660c1b
Walter E Brown <wb@fnal.gov>
2010-12-20 14:59:03 -0600

    ParameterSetRegistry now implemented via cet::registry_via_id.

--- fhiclcpp/ParameterSetRegistry.h
+++ fhiclcpp/ParameterSetRegistry.h
@@ -7,32 +7,16 @@
 //
 // ======================================================================
 
-#include "cetlib/registry.h"
+#include "cetlib/registry_via_id.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "fhiclcpp/ParameterSetID.h"
 
 namespace fhicl {
-  class ParameterSetRegistry;
+
+  typedef  cet::registry_via_id<ParameterSetID, ParameterSet>
+           ParameterSetRegistry;
+
 }
-
-// ----------------------------------------------------------------------
-
-class fhicl::ParameterSetRegistry
-  : public cet::registry<ParameterSetID, ParameterSet>
-{
-  typedef  ParameterSet                   pset_t;
-  typedef  ParameterSetID                 psid_t;
-  typedef  cet::registry<psid_t, pset_t>  base_t;
-
-public:
-  using base_t::empty;
-  using base_t::size;
-  using base_t::get;
-
-  static  ParameterSetID
-    put( ParameterSet const &  );
-
-};  // ParameterSetRegistry
 
 // ======================================================================
 

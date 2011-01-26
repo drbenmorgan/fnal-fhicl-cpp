52350032c34ee084d858b9590753adaf117890c6
Walter E Brown <wb@fnal.gov>
2011-01-26 16:04:34 -0600

    Add utility to dispay expanded fhicl files.

---
+++ fhiclcpp/fhicl-expand.cc
@@ -0,0 +1,63 @@
+// ======================================================================
+//
+// fhicl-expand: read/write specified text, replacing each #include
+//               directive with the corresponding file's contents
+//
+// ======================================================================
+
+#include "cetlib/include.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+// ----------------------------------------------------------------------
+
+static  std::string const &
+  fhicl_env_var( )
+{
+  static  std::string const  fhicl_env_var("FHICL_FILE_PATH");
+  return fhicl_env_var;
+}
+
+// ----------------------------------------------------------------------
+
+bool
+  is_single_dash( char const * s ) { return s[0] == '-' && s[1] == '\0'; }
+
+// ----------------------------------------------------------------------
+
+int
+  do_including( std::istream & from, std::ostream & to )
+{
+  std::string result;
+  if( ! cet::include(from, fhicl_env_var(), result) )
+    return 1;
+
+  to << result;
+  return 0;
+
+}  // do_including()
+
+// ----------------------------------------------------------------------
+
+int
+  main( int argc, char* argv[] )
+{
+  int nfailures = 0;
+
+  if( argc == 1 )
+    nfailures += do_including(std::cin, std::cout);
+  else {
+    for( int k = 1; k != argc; ++k ) {
+      if( is_single_dash(argv[k]) )
+        nfailures += do_including(std::cin, std::cout);
+      else {
+        std::ifstream from(argv[k], std::ios_base::in);
+        nfailures += do_including(from, std::cout);
+      }
+    }
+  }
+
+  return nfailures;
+
+}  // main()

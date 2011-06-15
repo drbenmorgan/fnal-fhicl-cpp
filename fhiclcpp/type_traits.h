2c6225af6ba4046232d890f380f7c1bd007b7b7f
Walter E Brown <wb@fnal.gov>
2011-06-15 14:11:58 -0500

    Simplify, mostly by appealing to cpp0x/type_traits.

--- fhiclcpp/type_traits.h
+++ fhiclcpp/type_traits.h
@@ -7,66 +7,34 @@
 //
 // ======================================================================
 
-#include "boost/type_traits/integral_constant.hpp"
-#include "boost/type_traits/is_floating_point.hpp"
-#include "boost/type_traits/remove_cv.hpp"
+#include "cpp0x/type_traits"
 #include <complex>
 
 // ======================================================================
 
 namespace tt {
-  using boost::true_type;
-  using boost::false_type;
-  using boost::is_floating_point;
+  using std::is_floating_point;
 
-  template< bool, class, class > struct conditional;
-  template< bool, class = void > struct enable_if;
+  using std::enable_if;
   template< bool, class = void > struct disable_if;
 
   template< class > struct is_complex;
   template< class > struct is_int;
   template< class > struct is_numeric;
   template< class > struct is_uint;
-
-  template< class T
-          , bool = is_int           <T>::value
-          , bool = is_uint          <T>::value
-          , bool = is_floating_point<T>::value
-          >
-    struct largest_type;
 }
 
 // ======================================================================
 
 namespace tt {
-  template< bool b, class T, class >
-    struct conditional            { typedef  T  type; };
-  template< class T, class F >
-    struct conditional<false,T,F> { typedef  F  type; };
-}
-
-// ----------------------------------------------------------------------
-
-namespace tt {
   template< bool b, class T >
-    struct enable_if          { typedef  T  type; };
-  template< class T >
-    struct enable_if<false,T> { };
+    struct disable_if : std::enable_if<!b,T>  { };
 }
 
 // ----------------------------------------------------------------------
 
 namespace tt {
-  template< bool b, class T >
-    struct disable_if          { };
-  template< class T >
-    struct disable_if<false,T> { typedef  T  type; };
-}
-
-// ----------------------------------------------------------------------
-
-namespace tt {
-  template< class T > struct is_complex : public false_type { };
+  template< class T > struct is_complex : public std::false_type { };
   template< class T >
     struct is_complex<T const> : public is_complex<T> { };
 
@@ -77,52 +45,37 @@ namespace tt {
 // ----------------------------------------------------------------------
 
 namespace tt {
-  template< class T > struct is_int : public false_type { };
+  template< class T > struct is_int : public std::false_type { };
   template< class T > struct is_int<T const> : public is_int<T> { };
 
-  template<> struct is_int<signed char> : public true_type { };
-  template<> struct is_int<short      > : public true_type { };
-  template<> struct is_int<int        > : public true_type { };
-  template<> struct is_int<long       > : public true_type { };
-  template<> struct is_int<long long  > : public true_type { };
+  template<> struct is_int<signed char> : public std::true_type { };
+  template<> struct is_int<short      > : public std::true_type { };
+  template<> struct is_int<int        > : public std::true_type { };
+  template<> struct is_int<long       > : public std::true_type { };
 }
 
 // ----------------------------------------------------------------------
 
 namespace tt {
-  template< class T > struct is_uint : public false_type { };
+  template< class T > struct is_uint : public std::false_type { };
   template< class T > struct is_uint<T const> : public is_uint<T> { };
 
-  template<> struct is_int<unsigned char     > : public true_type { };
-  template<> struct is_int<unsigned short    > : public true_type { };
-  template<> struct is_int<unsigned int      > : public true_type { };
-  template<> struct is_int<unsigned long     > : public true_type { };
-  template<> struct is_int<unsigned long long> : public true_type { };
+  template<> struct is_uint<unsigned char     > : public std::true_type { };
+  template<> struct is_uint<unsigned short    > : public std::true_type { };
+  template<> struct is_uint<unsigned int      > : public std::true_type { };
+  template<> struct is_uint<unsigned long     > : public std::true_type { };
 }
 
 // ----------------------------------------------------------------------
 
 namespace tt {
   template< class T > struct is_numeric
-    : public conditional<    is_int  <T>::value || is_uint   <T>::value
-                          || is_floating_point<T>::value
-                        , true_type
-                        , false_type
-                       >::type
+    : public std::integral_constant< bool
+                                   ,    is_int           <T>::value
+                                     || is_uint          <T>::value
+                                     || is_floating_point<T>::value
+                                   >
   { };
-}
-
-// ----------------------------------------------------------------------
-
-namespace tt {
-  template< class T > struct largest_type<T,true,false,false>
-  { typedef  long long  type; };
-
-  template< class T > struct largest_type<T,false,true,false>
-  { typedef  long long  type; };
-
-  template< class T > struct largest_type<T,false,false,true>
-  { typedef  long double  type; };
 }
 
 // ======================================================================

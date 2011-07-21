a7040e79f28623e2d8685c524c2f9d4a06ab084f
Walter E Brown <wb@fnal.gov>
2011-07-21 17:48:57 -0500

    Fix test's input; make test more meaningful.

--- test/test_suite.cc
+++ test/test_suite.cc
@@ -2,50 +2,53 @@
 
 #include "boost/test/auto_unit_test.hpp"
 
-#include "fhiclcpp/intermediate_table.h"
-#include "fhiclcpp/parse.h"
 #include "fhiclcpp/ParameterSet.h"
+#include "fhiclcpp/intermediate_table.h"
 #include "fhiclcpp/make_ParameterSet.h"
-#include <string>
+#include "fhiclcpp/parse.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace fhicl;
 using namespace std;
 
-// Contents from file
-string content("");
-
-// Parsed parameter sets
-ParameterSet ps1;
-ParameterSet ps2;
-
-// Function to read contents from a file
-   void readFile(string fileName, string & content)
-   {
-      ifstream in;
-      string line("");
-      in.open(fileName.c_str());
-      while(!in.eof() && in.good())
-      {
-         getline(in, line);
-
-         // Append newline since getline is delimeted by newline.
-         content.append("\n");
-         content.append(line);
-     }
-   } // readFile()
+void
+  readFile( string fileName, string & content )
+{
+  ifstream in(fileName.c_str());
+  for( string line; getline(in, line);  )
+    content.append(line).append("\n");
+}
 
 BOOST_AUTO_TEST_SUITE( document_test )
+
 BOOST_AUTO_TEST_CASE( doc )
 {
-   ofstream out("temp.txt");
-   readFile(boost::unit_test::framework::master_test_suite().argv[1], content);
-   intermediate_table table;
-   parse_document(content, table);
-   make_ParameterSet(table, ps1);
-   make_ParameterSet(table, ps2);
-   cout << ps1.to_string() << endl;
-   BOOST_CHECK_EQUAL(ps1, ps2);
-} // main()
-BOOST_AUTO_TEST_SUITE_END()
+  ParameterSet ps1;
+  {
+    string content;
+    readFile(boost::unit_test::framework::master_test_suite().argv[1], content);
+    intermediate_table table;
+    parse_document(content, table);
+    make_ParameterSet(table, ps1);
+  }
+  cout << "==========  Parsed parameter set ==========\n"
+       << ps1.to_indented_string()
+       << "==========  End of parameter set =========="
+       << endl;
 
+  ParameterSet ps2;
+  {
+    ostringstream os;
+    os << ps1.to_string() << endl;
+    intermediate_table table;
+    parse_document(os.str(), table);
+    make_ParameterSet(table, ps2);
+  }
+
+  BOOST_CHECK_EQUAL(ps1, ps2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()

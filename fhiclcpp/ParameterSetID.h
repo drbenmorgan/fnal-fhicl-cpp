b5836c13bd1d45a5e0a505377e24198418c72802
Chris Green <artists@fnal.gov>
2012-07-05 10:11:47 -0500

    Sidestep BOOST 1.49.0 / GCC 4.7.1 problems.

--- fhiclcpp/ParameterSetID.h
+++ fhiclcpp/ParameterSetID.h
@@ -8,7 +8,7 @@
 // ======================================================================
 
 #include "cetlib/sha1.h"
-#include "cpp0x/string"
+#include <string>
 #include "fhiclcpp/fwd.h"
 #include <ostream>
 

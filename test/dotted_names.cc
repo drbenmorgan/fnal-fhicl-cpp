ea9f66ed07a77e2e5f93ca3344b62ff45fa383f6
Marc Paterno <paterno@fnal.gov>
2012-08-24 11:57:58 -0500

    New test for dotted names

---
+++ test/dotted_names.cc
@@ -0,0 +1,14 @@
+#include "fhiclcpp/make_ParameterSet.h"
+#include "fhiclcpp/ParameterSet.h"
+#include <cassert>
+#include <string>
+
+int main()
+{
+  fhicl::ParameterSet ps;
+  std::string text("m1: { val: 3 name: boo} ");
+  fhicl::make_ParameterSet(text, ps);
+
+  assert(ps.get<int>("m1.val") == 3);
+  assert(ps.get<std::string>("m1.name") == "boo");
+}

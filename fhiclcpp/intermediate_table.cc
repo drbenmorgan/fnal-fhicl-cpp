eb751849a2e76e513d68304fae98d62000561f92
Chris Green <artists@fnal.gov>
2012-09-06 12:20:12 -0500

    Make config post-processing easier with new interface.

--- fhiclcpp/intermediate_table.cc
+++ fhiclcpp/intermediate_table.cc
@@ -26,21 +26,21 @@ typedef  intermediate_table::const_iterator  const_iterator;
 // ----------------------------------------------------------------------
 
 static extended_value const &
-  nil_item( )
+nil_item()
 {
   static extended_value const nil_item(false, NIL, std::string("nil"));
   return nil_item;
 }
 
 static extended_value const &
-  empty_seq( )
+empty_seq()
 {
   static extended_value const empty_seq(false, SEQUENCE, sequence_t());
   return empty_seq;
 }
 
 static extended_value const &
-  empty_tbl( )
+empty_tbl()
 {
   static extended_value const empty_tbl(false, TABLE, table_t());
   return empty_tbl;
@@ -48,197 +48,229 @@ static extended_value const &
 
 // ----------------------------------------------------------------------
 
-  intermediate_table::
-  intermediate_table( )
-: ex_val(false, TABLE, table_t() )
+intermediate_table::
+intermediate_table()
+  : ex_val(false, TABLE, table_t())
 { }
 
 // ----------------------------------------------------------------------
 
 const_iterator
-  intermediate_table::
-  begin( ) const
+intermediate_table::
+begin() const
 { return boost::any_cast<table_t const &>(ex_val.value).begin(); }
 
 const_iterator
-  intermediate_table::
-  end  ( ) const
+intermediate_table::
+end() const
 { return boost::any_cast<table_t const &>(ex_val.value).end(); }
 
 // ----------------------------------------------------------------------
 
 bool
-  intermediate_table::
-  empty( ) const
+intermediate_table::
+empty() const
 { return boost::any_cast<table_t const &>(ex_val.value).empty(); }
 
 // ----------------------------------------------------------------------
 
 void
-  intermediate_table::
-  insert( std::string const & name
-        , bool                in_prolog
-        , value_tag           tag
-        , boost::any const &  value
-        )
+intermediate_table::
+insert(std::string const & name
+       , bool                in_prolog
+       , value_tag           tag
+       , boost::any const  & value
+      )
 { insert(name, extended_value(in_prolog, tag, value)); }
 
 void
-  intermediate_table::
-  insert( std::string    const & name
-        , extended_value const & value
-        )
+intermediate_table::
+insert(std::string    const & name
+       , extended_value const & value
+      )
 {
-  if( ! value.in_prolog )  {
+  if (! value.in_prolog)  {
     table_t & t = boost::any_cast<table_t &>(ex_val.value);
     std::vector<std::string> const & key = split(name);
     iterator it = t.find(key[0]);
-    if( it != t.end() && it->second.in_prolog )
-      t.erase(it);
+    if (it != t.end() && it->second.in_prolog)
+    { t.erase(it); }
   }
-
   this->operator[](name)  = value;
 }
 
 // ----------------------------------------------------------------------
 
 extended_value &
-  intermediate_table::
-  operator [] ( std::string const & name )
+intermediate_table::
+operator [](std::string const & name)
 {
   std::vector<std::string> const & key = split(name);
   extended_value * p = & ex_val;
-
-  for( unsigned k = 0, sz = key.size(); k != sz; ++k ) {
+  for (unsigned k = 0, sz = key.size(); k != sz; ++k) {
     std::string const & this_key = key[k];
-    if( this_key.empty() )
+    if (this_key.empty())
       ;
-
-    else if( std::isdigit(this_key[0]) ) {
-      if( p->is_a(NIL) )
-        *p = empty_seq();
-      if( ! p->is_a(SEQUENCE) )
+    else if (std::isdigit(this_key[0])) {
+      if (p->is_a(NIL))
+      { *p = empty_seq(); }
+      if (! p->is_a(SEQUENCE))
         throw exception(cant_find, name)
-          << "-- not a sequence (at part \"" << this_key << "\")";
+            << "-- not a sequence (at part \"" << this_key << "\")";
       sequence_t & s = boost::any_cast<sequence_t &>(p->value);
       unsigned i = std::atoi(this_key.c_str());
-      while( s.size() <= i )
-        s.push_back( nil_item() );
+      while (s.size() <= i)
+      { s.push_back(nil_item()); }
       p = & s[i];
     }
-
-    else /* this_key[0] is alpha or '_' */ {
-      if( p->is_a(NIL) )
-        *p = empty_tbl();
-      if( ! p->is_a(TABLE) )
+    else { /* this_key[0] is alpha or '_' */
+      if (p->is_a(NIL))
+      { *p = empty_tbl(); }
+      if (! p->is_a(TABLE))
         throw exception(cant_find, name)
-          << "-- not a table (at part \"" << this_key << "\")";
+            << "-- not a table (at part \"" << this_key << "\")";
       table_t & t = boost::any_cast<table_t &>(p->value);
       iterator it = t.find(this_key);
-      if( it == t.end() ) {
-        t.insert( std::make_pair(this_key, nil_item()) );
+      if (it == t.end()) {
+        t.insert(std::make_pair(this_key, nil_item()));
         it = t.find(this_key);
       }
       p = & it->second;
     }
   }  // for
-
   return *p;
-
 }  // operator [] ()
 
 // ----------------------------------------------------------------------
 
 extended_value const &
-  intermediate_table::
-  find( std::string const & name ) const
+intermediate_table::
+find(std::string const & name) const
 {
   std::vector<std::string> const & key = split(name);
   extended_value const * p = & ex_val;
-
-  for( unsigned k = 0, sz = key.size(); k != sz; ++k ) {
+  for (unsigned k = 0, sz = key.size(); k != sz; ++k) {
     std::string const & this_key = key[k];
-    if( this_key.empty() )
+    if (this_key.empty())
       ;
-
-    else if( std::isdigit(this_key[0]) ) {
-      if( ! p->is_a(SEQUENCE) )
+    else if (std::isdigit(this_key[0])) {
+      if (! p->is_a(SEQUENCE))
         throw exception(cant_find, name)
-          << "-- not a sequence (at part \"" << this_key << "\")";
+            << "-- not a sequence (at part \"" << this_key << "\")";
       sequence_t const & s = boost::any_cast<sequence_t const &>(p->value);
       unsigned i = std::atoi(this_key.c_str());
-      if( s.size() <= i )
+      if (s.size() <= i)
         throw exception(cant_find, name)
-          << "(at part \"" << this_key << "\")";
+            << "(at part \"" << this_key << "\")";
       p = & s[i];
     }
-
-    else /* this_key[0] is alpha or '_' */ {
-      if( ! p->is_a(TABLE) )
+    else { /* this_key[0] is alpha or '_' */
+      if (! p->is_a(TABLE))
         throw exception(cant_find, name)
-          << "-- not a table (at part \"" << this_key << "\")";
+            << "-- not a table (at part \"" << this_key << "\")";
       table_t const & t = boost::any_cast<table_t const &>(p->value);
       const_iterator it = t.find(this_key);
-      if( it == t.end() )
+      if (it == t.end())
         throw exception(cant_find, name)
-          << "(at part \"" << this_key << "\")";
+            << "(at part \"" << this_key << "\")";
       p = & it->second;
     }
   }  // for
-
   return *p;
-
 }  // find()
 
 // ----------------------------------------------------------------------
 
 bool
-  intermediate_table::
-  exists( std::string const & name ) const
+intermediate_table::
+exists(std::string const & name) const
 {
   std::vector<std::string> const & key = split(name);
   extended_value const * p = & ex_val;
-
-  for( unsigned k = 0, sz = key.size(); k != sz; ++k ) {
+  for (unsigned k = 0, sz = key.size(); k != sz; ++k) {
     std::string const & this_key = key[k];
-    if( this_key.empty() )
+    if (this_key.empty())
       ;
-
-    else if( std::isdigit(this_key[0]) ) {
-      if( ! p->is_a(SEQUENCE) )
-        return false;
+    else if (std::isdigit(this_key[0])) {
+      if (! p->is_a(SEQUENCE))
+      { return false; }
       sequence_t const & s = boost::any_cast<sequence_t const &>(p->value);
       unsigned i = std::atoi(this_key.c_str());
-      if( s.size() <= i )
-        return false;
+      if (s.size() <= i)
+      { return false; }
       p = & s[i];
     }
-
-    else /* this_key[0] is alpha or '_' */ {
-      if( ! p->is_a(TABLE) )
-        return false;
+    else { /* this_key[0] is alpha or '_' */
+      if (! p->is_a(TABLE))
+      { return false; }
       table_t const & t = boost::any_cast<table_t const &>(p->value);
       const_iterator it = t.find(this_key);
-      if( it == t.end() )
-        return false;
+      if (it == t.end())
+      { return false; }
       p = & it->second;
     }
   }  // for
-
   return true;
-
 }  // exists()
+
+void
+intermediate_table::
+erase(std::string const & name)
+{
+  // A missing part of the "a.b.c.d" chain will not cause an error; it
+  // is an error for an intermediate link in the chain *not* to be a
+  // table.
+  auto const & key(split(name));
+  auto p(& ex_val);
+  auto t(boost::any_cast<table_t>(&p->value));
+  auto it(t->end());
+  bool at_sequence(false);
+for (auto const & this_key : key) {
+    if (this_key.empty())
+      ;
+    else if (std::isdigit(this_key[0])) {
+      if (! p->is_a(SEQUENCE))
+        throw exception(cant_find, name)
+            << "-- not a sequence (at part \"" << this_key << "\")";
+      auto & s = boost::any_cast<sequence_t &>(p->value);
+      unsigned i = std::atoi(this_key.c_str());
+      if (s.size() <= i) {
+        return;
+      }
+      p = & s[i];
+      at_sequence = true;
+    }
+    else { /* this_key[0] is alpha or '_' */
+      if (! p->is_a(TABLE))
+        throw exception(cant_find, name)
+            << "-- not a table (at part \"" << this_key << "\")";
+      at_sequence = false;
+      t = boost::any_cast<table_t>(&p->value);
+      it = t->find(this_key);
+      if (it == t->end()) {
+        return;
+      }
+      p = & it->second;
+    }
+  }  // for
+  if (at_sequence) {
+    throw fhicl::exception(unimplemented, "erase sequence member");
+  }
+  if (it != t->end()) {
+    t->erase(it);
+  }
+}
 
 // ----------------------------------------------------------------------
 
 std::vector<std::string>
-  intermediate_table::
-  split( std::string const & name ) const
+intermediate_table::
+split(std::string const & name) const
 {
   std::vector<std::string> result;
-  boost::algorithm::split( result
-                         , name
-                         , boost::algorithm::is_any_of(".[]")
+  boost::algorithm::split(result
+                          , name
+                          , boost::algorithm::is_any_of(".[]")
                          );
   return result;
 }
--- fhiclcpp/intermediate_table.h
+++ fhiclcpp/intermediate_table.h
@@ -1,60 +1,290 @@
 #ifndef FHICLCPP_INTERMEDIATE_TABLE_H
 #define FHICLCPP_INTERMEDIATE_TABLE_H
 
-// ======================================================================
+////////////////////////////////////////////////////////////////////////
+// intermediate table: generic representation of parameters.
 //
-// intermediate_table
+// This class should only be used in the following two circumstances:
 //
-// ======================================================================
+// 1. Configuration post-processing, prior to ParameterSet creation
+//    (use the "simple interface" where possible).
+//
+// 2. Internally by FHiCL.
+//
+// In all other circumstances, ParameterSet is almost certainly more
+// appropriate.
+//
+// 2012/08/29 CG.
+////////////////////////////////////////////////////////////////////////
 
 #include "boost/any.hpp"
 #include "cpp0x/string"
+#include "fhiclcpp/coding.h"
 #include "fhiclcpp/extended_value.h"
+#include "fhiclcpp/type_traits.h"
 #include "fhiclcpp/fwd.h"
+#include <complex>
 #include <map>
 #include <vector>
 
 // ----------------------------------------------------------------------
 
-class fhicl::intermediate_table
-{
+class fhicl::intermediate_table {
 public:
-  typedef  extended_value::atom_t      atom_t;
-  typedef  extended_value::complex_t   complex_t;
-  typedef  extended_value::sequence_t  sequence_t;
-  typedef  extended_value::table_t     table_t;
+  // Constructor
+  intermediate_table();
 
-  typedef  table_t::iterator           iterator;
-  typedef  table_t::const_iterator     const_iterator;
+  ////////////////////
+  // Simple interface:
+  bool empty() const;
 
-  intermediate_table( );
+  bool exists(std::string const & name) const;
 
-  const_iterator  begin( ) const;
-  const_iterator  end  ( ) const;
+  void erase(std::string const & name);
 
-  bool  empty( ) const;
+  template <typename T>
+  T get(std::string const & name);
 
-  void  insert( std::string const & name
-              , bool                in_prolog
-              , value_tag           tag
-              , boost::any const &  value
+  void put(std::string const & name,
+           std::string const & value, // String.
+           bool in_prolog = false);
+  void put(std::string const & name,
+           char const * value, // String.
+           bool in_prolog = false);
+  void put(std::string const & name,
+           bool value, // Boolean.
+           bool in_prolog = false);
+  template <typename T>
+  void put(std::string const & name,
+           std::complex<T> const & value, // Complex.
+           bool in_prolog = false);
+  template <typename T>
+  void put(std::string const & name,
+           std::vector<T> const & value, // Sequence.
+           bool in_prolog = false);
+  template <typename T>
+  typename std::enable_if<tt::is_numeric<T>::value, void>::type
+  put(std::string const & name,
+      T value, // Number
+      bool in_prolog = false);
+
+  void putEmptySequence(std::string const & name,
+                        bool in_prolog = false); // Empty Sequence.
+  void putEmptyTable(std::string const & name,
+                     bool in_prolog = false); // Empty Table.
+  void putNil(std::string const & name,
+              bool in_prolog = false); // Nil.
+
+  ////////////////////
+  // Expert interface.
+  // Typedefs.
+  typedef extended_value::atom_t      atom_t;
+  typedef extended_value::complex_t   complex_t;
+  typedef extended_value::sequence_t  sequence_t;
+  typedef extended_value::table_t     table_t;
+  typedef table_t::iterator iterator;
+  typedef table_t::const_iterator const_iterator;
+
+  const_iterator  begin() const;
+  const_iterator  end() const;
+
+  // Flexible insert interface.
+  void  insert(std::string const & name
+               , bool                in_prolog
+               , value_tag           tag
+               , boost::any const  & value
               );
-  void  insert( std::string    const & name
-              , extended_value const & value
+  void  insert(std::string    const & name
+               , extended_value const & value
               );
 
-  extended_value &  operator [] ( std::string const & name );
-  extended_value const &  find( std::string const & name ) const;
-
-  bool  exists( std::string const & name ) const;
+  extended_value  & operator [](std::string const & name);
+  extended_value const  & find(std::string const & name) const;
 
 private:
   extended_value  ex_val;
 
-  std::vector<std::string>  split( std::string const & name ) const;
+  std::vector<std::string>  split(std::string const & name) const;
 
 };  // intermediate_table
 
-// ======================================================================
+namespace fhicl {
+  namespace detail {
+    template <typename T>
+    typename tt::disable_if<std::is_reference<T>::value || std::is_pointer<T>::value, T>::type
+    it_value_get(intermediate_table & table,
+                 std::string const & name)
+    {
+      T result;
+      detail::decode(table[name].value, result);
+      return result;
+    }
 
+    template <>
+    typename tt::disable_if<std::is_reference<intermediate_table::sequence_t>::value ||
+                            std::is_pointer<intermediate_table::sequence_t>::value,
+                            intermediate_table::sequence_t>::type
+    it_value_get<intermediate_table::sequence_t>(intermediate_table & table,
+                                                 std::string const & name) = delete;
+
+    template <>
+    typename tt::disable_if<std::is_reference<intermediate_table::table_t>::value ||
+                            std::is_pointer<intermediate_table::table_t>::value,
+                            intermediate_table::table_t>::type
+    it_value_get<intermediate_table::table_t>(intermediate_table & table,
+                                              std::string const & name) = delete;
+
+    template <typename T>
+    typename tt::enable_if<std::is_reference<T>::value ||
+                           std::is_pointer<T>::value, T>::type
+    it_value_get(intermediate_table & table,
+                 std::string const & name) = delete;
+
+    template <>
+    inline
+    typename tt::enable_if<std::is_reference<intermediate_table::sequence_t &>::value,
+                           intermediate_table::sequence_t &>::type
+    it_value_get<intermediate_table::sequence_t &> (intermediate_table & table,
+                                                    std::string const & name)
+    {
+      return boost::any_cast<intermediate_table::sequence_t &>(table[name].value);
+    }
+
+    template <>
+    inline
+    typename tt::enable_if<std::is_reference<intermediate_table::table_t &>::value,
+                           intermediate_table::table_t &>::type
+    it_value_get<intermediate_table::table_t &> (intermediate_table & table,
+                                                 std::string const & name)
+    {
+      return boost::any_cast<intermediate_table::table_t &>(table[name].value);
+    }
+
+    template <>
+    inline
+    typename tt::enable_if<std::is_reference<intermediate_table::sequence_t const &>::value,
+                           intermediate_table::sequence_t const &>::type
+    it_value_get<intermediate_table::sequence_t const &> (intermediate_table & table,
+                                                    std::string const & name)
+    {
+      return boost::any_cast<intermediate_table::sequence_t const &>(table[name].value);
+    }
+
+    template <>
+    inline
+    typename tt::enable_if<std::is_reference<intermediate_table::table_t const &>::value,
+                           intermediate_table::table_t const &>::type
+    it_value_get<intermediate_table::table_t const &> (intermediate_table & table,
+                                                 std::string const & name)
+    {
+      return boost::any_cast<intermediate_table::table_t const &>(table[name].value);
+    }
+  }
+}
+
+template <typename T>
+inline
+T
+fhicl::intermediate_table::
+get(std::string const & name)
+{
+  return detail::it_value_get<T>(* this, name);
+}
+
+inline
+void
+fhicl::intermediate_table::
+put(std::string const & name,
+       std::string const & value, // String.
+       bool in_prolog)
+{
+  insert(name, in_prolog, STRING, detail::encode(value));
+}
+
+inline
+void
+fhicl::intermediate_table::
+put(std::string const & name,
+       char const * value, // String.
+       bool in_prolog)
+{
+  insert(name, in_prolog, STRING, detail::encode(value));
+}
+
+inline
+void
+fhicl::intermediate_table::
+put(std::string const & name,
+       bool value, // Boolean.
+       bool in_prolog)
+{
+  insert(name, in_prolog, BOOL, detail::encode(value));
+}
+
+template <typename T>
+void
+fhicl::intermediate_table::
+put(std::string const & name,
+       std::complex<T> const & value, // Complex.
+       bool in_prolog)
+{
+  insert(name, in_prolog, COMPLEX, detail::encode(value));
+}
+
+template <typename T>
+inline
+void
+fhicl::intermediate_table::
+put(std::string const & name,
+       std::vector<T> const & value, // Sequence.
+       bool in_prolog)
+{
+  putEmptySequence(name, in_prolog);
+  size_t count(0);
+  for (auto const & item : value) {
+    put(name + "[" + std::to_string(count++) + "]", item, in_prolog);
+  }
+}
+
+template <typename T>
+inline
+typename std::enable_if<tt::is_numeric<T>::value, void>::type
+fhicl::intermediate_table::
+put(std::string const & name,
+       T value, // Number
+       bool in_prolog)
+{
+  insert(name, in_prolog, NUMBER, detail::encode(value));
+}
+
+inline
+void
+fhicl::intermediate_table::
+putEmptySequence(std::string const & name,
+                    bool in_prolog) // Sequence.
+{
+  insert(name, in_prolog, SEQUENCE, sequence_t());
+}
+
+inline
+void
+fhicl::intermediate_table::
+putEmptyTable(std::string const & name,
+                    bool in_prolog) // Table.
+{
+  insert(name, in_prolog, TABLE, table_t());
+}
+
+inline
+void
+fhicl::intermediate_table::
+putNil(std::string const & name,
+          bool in_prolog) // Nil.
+{
+  insert(name, in_prolog, NIL, detail::encode((void *)0));
+}
 #endif
+
+// Local Variables:
+// mode: c++
+// End:

8cfb3611ff867535e5f1ea4785b6474f166ed07e
Chris Green <artists@fnal.gov>
2015-02-05 11:24:19 -0600

    Run astyle on ParameterSet.{h,cc}

--- fhiclcpp/ParameterSet.cc
+++ fhiclcpp/ParameterSet.cc
@@ -17,125 +17,120 @@ using namespace std;
 using boost::any;
 using boost::any_cast;
 
-typedef  ParameterSet::ps_atom_t      ps_atom_t;
-typedef  ParameterSet::ps_sequence_t  ps_sequence_t;
+typedef ParameterSet::ps_atom_t ps_atom_t;
+typedef ParameterSet::ps_sequence_t ps_sequence_t;
 
-typedef  long double  ldbl;
+typedef long double ldbl;
 
 // ======================================================================
 
 string
-ParameterSet::stringify_( any const & a, bool compact ) const
+ParameterSet::stringify_(any const & a, bool compact) const
 {
   string result;
-  if( is_table(a) ) {
+  if (is_table(a)) {
     ParameterSetID const & psid = any_cast<ParameterSetID>(a);
     result = '{' + ParameterSetRegistry::get(psid).to_string() + '}';
-    if (compact && result.size() > ( 5 + ParameterSetID::max_str_size())) {
+    if (compact && result.size() > (5 + ParameterSetID::max_str_size())) {
       // Replace with a reference to the ParameterSetID;
       result = std::string("@id::") + psid.to_string();
     }
   }
-
-  else if( is_sequence(a) ) {
+  else if (is_sequence(a)) {
     ps_sequence_t const & seq = any_cast<ps_sequence_t>(a);
     result = '[';
-    if( ! seq.empty() ) {
-      result.append(stringify_(*seq.begin(),compact));
-      for( ps_sequence_t::const_iterator it = seq.begin()
-                                       , e  = seq.end(); ++it != e;  ) {
+    if (! seq.empty()) {
+      result.append(stringify_(*seq.begin(), compact));
+      for (ps_sequence_t::const_iterator it = seq.begin()
+                                              , e = seq.end(); ++it != e;) {
         result.append(1, ',')
-          .append(stringify_(*it,compact));
+        .append(stringify_(*it, compact));
       }
     }
     result.append(1, ']');
   }
-
-  else  {  // is_atom(a)
+  else { // is_atom(a)
     ps_atom_t str = any_cast<ps_atom_t>(a);
-    result = str == string(9, '\0')  ? "@nil" : str;
+    result = str == string(9, '\0') ? "@nil" : str;
   }
-
   return result;
-}  // stringify_()
+} // stringify_()
 
 // ----------------------------------------------------------------------
 
 bool
-  ParameterSet::is_empty( ) const
+ParameterSet::is_empty() const
 { return mapping_.empty(); }
 
 ParameterSetID
-  ParameterSet::id( ) const
+ParameterSet::id() const
 {
-  if( ! id_.is_valid() )
-    id_.reset(*this);
+  if (! id_.is_valid())
+  { id_.reset(*this); }
   return id_;
 }
 
 string
-ParameterSet::to_string_( bool compact ) const
+ParameterSet::to_string_(bool compact) const
 {
   string result;
-  if( mapping_.empty() )
-    return result;
-
+  if (mapping_.empty())
+  { return result; }
   map_iter_t it = mapping_.begin();
-  result.append( it->first )
-    .append( 1, ':' )
-    .append( stringify_(it->second, compact) )
+  result.append(it->first)
+  .append(1, ':')
+  .append(stringify_(it->second, compact))
+  ;
+  for (map_iter_t const e = mapping_.end(); ++it != e;)
+    result.append(1, ' ')
+    .append(it->first)
+    .append(1, ':')
+    .append(stringify_(it->second, compact))
     ;
-  for( map_iter_t const e = mapping_.end(); ++it != e; )
-    result.append( 1, ' ' )
-      .append( it->first )
-      .append( 1, ':' )
-      .append( stringify_(it->second, compact) )
-      ;
-
   return result;
 }
 
 vector<string>
-  ParameterSet::get_keys( ) const
+ParameterSet::get_keys() const
 {
-  vector<string> keys;  keys.reserve( mapping_.size() );
-  for( map_iter_t it = mapping_.begin()
-                , e  = mapping_.end(); it != e; ++it )
-    keys.push_back( it->first );
+  vector<string> keys; keys.reserve(mapping_.size());
+  for (map_iter_t it = mapping_.begin()
+                       , e = mapping_.end(); it != e; ++it)
+  { keys.push_back(it->first); }
   return keys;
 }
 
 vector<string>
-  ParameterSet::get_pset_keys( ) const
+ParameterSet::get_pset_keys() const
 {
   vector<string> keys;
-  for( map_iter_t it = mapping_.begin()
-                , e  = mapping_.end(); it != e; ++it )
-    if( is_table(it->second) )
-      keys.push_back( it->first );
+  for (map_iter_t it = mapping_.begin()
+                       , e = mapping_.end(); it != e; ++it)
+    if (is_table(it->second))
+    { keys.push_back(it->first); }
   return keys;
 }
 
 // ----------------------------------------------------------------------
 
 void
-  ParameterSet::put( std::string const & key )
+ParameterSet::put(std::string const & key)
 {
   void * const value = nullptr;
-  put(key, value );
+  put(key, value);
 }
 
 // ----------------------------------------------------------------------
 
 void
-  ParameterSet::insert( string const & key, any const & value )
+ParameterSet::insert(string const & key, any const & value)
 {
   mapping_.emplace(key, value);
   id_.invalidate();
 }
 
 bool
-  ParameterSet::erase( string const & key )
+ParameterSet::erase(string const & key)
 {
   bool const did_erase = (1u == mapping_.erase(key));
   id_.invalidate();
@@ -147,130 +142,123 @@ ParameterSet::key_is_type_(std::string const & key,
                            std::function<bool (boost::any const &)> func) const
 {
   if (key.find('.') != std::string::npos) {
-    throw fhicl::exception(unimplemented, "is_{table,sequence,atom}() for nested key.");
+    throw fhicl::exception(unimplemented,
+                           "is_{table,sequence,atom}() for nested key.");
   }
   map_iter_t it = mapping_.find(key);
-  if( it == mapping_.end() )
-    throw exception(error::cant_find, key);
+  if (it == mapping_.end())
+  { throw exception(error::cant_find, key); }
   return func(it->second);
 }
 
 // ======================================================================
 
-class ParameterSet::Prettifier
-{
+class ParameterSet::Prettifier {
 public:
-  Prettifier( map_t const & mapping,
-              unsigned initial_indent_level  = 0)
-    : mapping_( mapping )
-    , result_ ( )
-    , initial_indent_ ( initial_indent_level * INDENT_PER )
+  Prettifier(map_t const & mapping,
+             unsigned initial_indent_level = 0)
+    : mapping_(mapping)
+    , result_()
+    , initial_indent_(initial_indent_level * INDENT_PER)
   { }
 
   string
-    operator () ( )
+  operator()()
   {
-    for( map_iter_t it = mapping_.begin()
-                  , e  = mapping_.end();  it != e;  ++it ) {
-      assert( next_col() == 1u );
+    for (map_iter_t it = mapping_.begin()
+                         , e = mapping_.end(); it != e; ++it) {
+      assert(next_col() == 1u);
       goto_col(initial_indent_ + 1);
-      result_.append( it->first )
-             .append( ": " )
-             ;
-      stringify( it->second );
-      result_.append( "\n" );
+      result_.append(it->first)
+      .append(": ")
+      ;
+      stringify(it->second);
+      result_.append("\n");
     }
-
     return std::move(result_);
   }
 
 private:
   static constexpr unsigned INDENT_PER = 2;
 
-  map_t const &  mapping_;
-  string         result_;
-  unsigned       initial_indent_;
+  map_t const & mapping_;
+  string result_;
+  unsigned initial_indent_;
 
-  size_t  next_col( ) const
+  size_t next_col() const
   {
-    size_t  ans     = result_.size() + 1u;
-    size_t  last_nl = result_.find_last_of('\n') + 1u;
+    size_t ans = result_.size() + 1u;
+    size_t last_nl = result_.find_last_of('\n') + 1u;
     return ans - last_nl;
   }
 
   void
-    goto_col( size_t dest )
+  goto_col(size_t dest)
   {
     size_t now_at = next_col();
-    if( now_at > dest ) {
-      result_.append( "\n" );
+    if (now_at > dest) {
+      result_.append("\n");
       now_at = 1;
     }
-    result_.append( dest-now_at, ' ' );
+    result_.append(dest - now_at, ' ');
   }
 
   void
-    stringify( any const & a )
+  stringify(any const & a)
   {
     size_t col = next_col();
-
-    if( is_table(a) ) {
-      ParameterSetID const & psid    = any_cast<ParameterSetID>(a);
-      ParameterSet   const & ps      = ParameterSetRegistry::get(psid);
-      map_t          const & mapping = ps.mapping_;
-
-      result_.append( "{" );
-      if( ! mapping.empty() ) {
+    if (is_table(a)) {
+      ParameterSetID const & psid = any_cast<ParameterSetID>(a);
+      ParameterSet const & ps = ParameterSetRegistry::get(psid);
+      map_t const & mapping = ps.mapping_;
+      result_.append("{");
+      if (! mapping.empty()) {
         // Emit 1st pair:
         map_iter_t it = mapping.begin();
-        result_.append( " " )
-               .append( it->first )
-               .append( ": " )
-               ;
-        stringify( it->second );
+        result_.append(" ")
+        .append(it->first)
+        .append(": ")
+        ;
+        stringify(it->second);
         // Emit remaining pairs:
-        for( map_iter_t const e  = mapping.end();  ++it != e;  ) {
-          goto_col( col+INDENT_PER );
-          result_.append( it->first )
-                 .append( ": " )
-                 ;
-          stringify( it->second );
+        for (map_iter_t const e = mapping.end(); ++it != e;) {
+          goto_col(col + INDENT_PER);
+          result_.append(it->first)
+          .append(": ")
+          ;
+          stringify(it->second);
         }
-        goto_col( col );
+        goto_col(col);
       }
-      result_.append( "}" );
+      result_.append("}");
     }
-
-    else if( is_sequence(a) ) {
+    else if (is_sequence(a)) {
       ps_sequence_t const & seq = any_cast<ps_sequence_t>(a);
-
-      result_.append( "[" );
-      if( ! seq.empty() ) {
-        result_.append( " " );
+      result_.append("[");
+      if (! seq.empty()) {
+        result_.append(" ");
         stringify(*seq.begin());
-        for( ps_sequence_t::const_iterator it = seq.begin()
-                                         , e  = seq.end(); ++it != e;  ) {
-          goto_col( col );
-          result_.append( ", " );
+        for (ps_sequence_t::const_iterator it = seq.begin()
+                                                , e = seq.end(); ++it != e;) {
+          goto_col(col);
+          result_.append(", ");
           stringify(*it);
         }
-        if( seq.size() == 1u )  result_.append( " " );
-        else                    goto_col( col );
+        if (seq.size() == 1u) { result_.append(" "); }
+        else { goto_col(col); }
       }
-      result_.append( "]" );
+      result_.append("]");
     }
+    else // is_atom(a)
+    { result_.append(any_cast<ps_atom_t>(a)); }
+  } // stringify()
 
-    else  // is_atom(a)
-      result_.append( any_cast<ps_atom_t>(a) );
-
-  }  // stringify()
-
-};  // Prettifier
+}; // Prettifier
 
 // ----------------------------------------------------------------------
 
 string
-  ParameterSet::to_indented_string(unsigned initial_indent_level) const
+ParameterSet::to_indented_string(unsigned initial_indent_level) const
 {
   Prettifier p(mapping_, initial_indent_level);
   return p();
--- fhiclcpp/ParameterSet.h
+++ fhiclcpp/ParameterSet.h
@@ -22,80 +22,79 @@
 
 // ----------------------------------------------------------------------
 
-class fhicl::ParameterSet
-{
+class fhicl::ParameterSet {
 public:
-  typedef  fhicl::detail::ps_atom_t           ps_atom_t;
-  typedef  fhicl::detail::ps_sequence_t       ps_sequence_t;
+  typedef fhicl::detail::ps_atom_t ps_atom_t;
+  typedef fhicl::detail::ps_sequence_t ps_sequence_t;
 
   // compiler generates default c'tor, d'tor, copy c'tor, copy assignment
 
   // observers:
-  bool                        is_empty           ( ) const;
-  ParameterSetID              id                 ( ) const;
-  std::string                 to_string          ( ) const;
-  std::string                 to_compact_string  ( ) const;
-  std::string                 to_indented_string ( unsigned initial_indent_level = 0 ) const;
-  std::vector<std::string>    get_keys           ( ) const;
-  std::vector<std::string>    get_pset_keys      ( ) const;
+  bool is_empty() const;
+  ParameterSetID id() const;
+  std::string to_string() const;
+  std::string to_compact_string() const;
+  std::string to_indented_string(unsigned initial_indent_level = 0) const;
+  std::vector<std::string> get_keys() const;
+  std::vector<std::string> get_pset_keys() const;
   // Key must be local to this parameter set: no nesting.
-  bool                        has_key            ( std::string const & key ) const;
-  bool                        is_key_to_table    ( std::string const & key ) const;
-  bool                        is_key_to_sequence ( std::string const & key ) const;
-  bool                        is_key_to_atom     ( std::string const & key ) const;
+  bool has_key(std::string const & key) const;
+  bool is_key_to_table(std::string const & key) const;
+  bool is_key_to_sequence(std::string const & key) const;
+  bool is_key_to_atom(std::string const & key) const;
 
   // retrievers:
   template< class T >
-    bool get_if_present( std::string const & key, T & value ) const;
+  bool get_if_present(std::string const & key, T & value) const;
   template< class T, class Via >
-    bool get_if_present( std::string const & key, T & value
-                       , T convert(Via const &) ) const;
+  bool get_if_present(std::string const & key, T & value
+                      , T convert(Via const &)) const;
   template< class T >
-    T  get( std::string const & key ) const;
+  T get(std::string const & key) const;
   template< class T, class Via >
-    T  get( std::string const & key
-          , T convert(Via const &) ) const;
+  T get(std::string const & key
+        , T convert(Via const &)) const;
   template< class T >
-    T  get( std::string const & key, T const & default_value ) const;
+  T get(std::string const & key, T const & default_value) const;
   template< class T, class Via >
-    T  get( std::string const & key, T const & default_value
-          , T convert(Via const &) ) const;
+  T get(std::string const & key, T const & default_value
+        , T convert(Via const &)) const;
 
   // inserters:
-  void  insert( std::string const & key, boost::any const & value );
-  void  put( std::string const & key );  // implicit nil value
+  void insert(std::string const & key, boost::any const & value);
+  void put(std::string const & key);   // implicit nil value
   template< class T >
-    void  put( std::string const & key, T const & value );
+  void put(std::string const & key, T const & value);
 
   // deleters:
-  bool  erase( std::string const & key );
+  bool erase(std::string const & key);
 
   // comparators:
-  bool  operator == ( ParameterSet const & other ) const;
-  bool  operator != ( ParameterSet const & other ) const;
+  bool operator == (ParameterSet const & other) const;
+  bool operator != (ParameterSet const & other) const;
 
 private:
-  typedef  std::map<std::string,boost::any>  map_t;
-  typedef  map_t::const_iterator             map_iter_t;
+  typedef std::map<std::string, boost::any> map_t;
+  typedef map_t::const_iterator map_iter_t;
 
-  map_t                    mapping_;
-  mutable  ParameterSetID  id_;
+  map_t mapping_;
+  mutable ParameterSetID id_;
 
-  std::string  to_string_( bool compact = false ) const;
-  std::string  stringify_( boost::any const & a,
-                           bool compact = false ) const;
+  std::string to_string_(bool compact = false) const;
+  std::string stringify_(boost::any const & a,
+                         bool compact = false) const;
 
   // Local retrieval only.
   template< class T >
-    bool get_one_( std::string const & key, T & value ) const;
+  bool get_one_(std::string const & key, T & value) const;
 
   bool
-    key_is_type_(std::string const & key,
-                 std::function<bool (boost::any const &)> func) const;
+  key_is_type_(std::string const & key,
+               std::function<bool (boost::any const &)> func) const;
 
   class Prettifier;
 
-};  // ParameterSet
+}; // ParameterSet
 
 // ======================================================================
 
@@ -147,32 +146,32 @@ bool
 fhicl::ParameterSet::
 is_key_to_atom(std::string const & key) const
 {
-  return key_is_type_(key, [](boost::any const & a)
-                      { return ! (detail::is_sequence(a) ||
-                                  detail::is_table(a));
-                      });
+  return key_is_type_(key, [](boost::any const & a) {
+    return !(detail::is_sequence(a) ||
+             detail::is_table(a));
+  });
 }
 
 template< class T >
-  void
-  fhicl::ParameterSet::put( std::string const & key, T const & value )
+void
+fhicl::ParameterSet::put(std::string const & key, T const & value)
 try
 {
-  insert(key, boost::any(detail::encode(value)) );
+  insert(key, boost::any(detail::encode(value)));
 }
-catch( boost::bad_lexical_cast const & e )
+catch (boost::bad_lexical_cast const & e)
 {
   throw fhicl::exception(cant_insert, key) << e.what();
 }
-catch( boost::bad_numeric_cast const & e )
+catch (boost::bad_numeric_cast const & e)
 {
   throw fhicl::exception(cant_insert, key) << e.what();
 }
-catch( fhicl::exception const & e )
+catch (fhicl::exception const & e)
 {
   throw fhicl::exception(cant_insert, key, e);
 }
-catch( std::exception const & e )
+catch (std::exception const & e)
 {
   throw fhicl::exception(cant_insert, key) << e.what();
 }
@@ -180,109 +179,108 @@ catch( std::exception const & e )
 // ----------------------------------------------------------------------
 
 template< class T >
-  bool
-  fhicl::ParameterSet::get_if_present( std::string const & key
-                                     , T                 & value
-                                     ) const
+bool
+fhicl::ParameterSet::get_if_present(std::string const & key
+                                    , T & value
+                                   ) const
 {
-  typedef  std::vector<std::string>  Keys;
-  Keys  keys;
-  boost::algorithm::split( keys
-                         , key
-                         , boost::algorithm::is_any_of(".")
+  typedef std::vector<std::string> Keys;
+  Keys keys;
+  boost::algorithm::split(keys
+                          , key
+                          , boost::algorithm::is_any_of(".")
                          );
-  Keys::iterator  b = keys.begin()
-               ,  e = keys.end()
-               ,  r = b;
-  for( ; b != e; ++b )
-    if( ! b->empty() )
-      *r++ = *b;
+  Keys::iterator b = keys.begin()
+                     , e = keys.end()
+                           , r = b;
+  for (; b != e; ++b)
+    if (! b->empty())
+    { *r++ = *b; }
   keys.erase(r, e);
-
-  if( keys.empty() )
-    throw fhicl::exception(cant_find, "vacuous key");
-
+  if (keys.empty())
+  { throw fhicl::exception(cant_find, "vacuous key"); }
   ParameterSet const * p = this;
   ParameterSet pset;
-  for( b = keys.begin(), e = keys.end() - 1; b != e; ++b ) {
+  for (b = keys.begin(), e = keys.end() - 1; b != e; ++b) {
     std::string const & this_key = *b;
-    if( std::isdigit(this_key[0]) )
-      throw fhicl::exception(unimplemented, "lookup in a sequence");
-    if( ! p->get_one_(this_key, pset) )
-      return false;
+    if (std::isdigit(this_key[0]))
+    { throw fhicl::exception(unimplemented, "lookup in a sequence"); }
+    if (! p->get_one_(this_key, pset))
+    { return false; }
     p = & pset;
   }
-
   return p->get_one_(keys.back(), value);
-}  // get_if_present<>()
+} // get_if_present<>()
 
 template< class T, class Via >
-  bool
-  fhicl::ParameterSet::get_if_present( std::string const & key
-                                     , T                 & result
-                                     , T convert(Via const &)
-                                     ) const
+bool
+fhicl::ParameterSet::get_if_present(std::string const & key
+                                    , T & result
+                                    , T convert(Via const &)
+                                   ) const
 {
   Via go_between;
-  if( ! get_if_present(key, go_between) )
-    return false;
+  if (! get_if_present(key, go_between))
+  { return false; }
   result = convert(go_between);
   return true;
-}  // get_if_present<>()
+} // get_if_present<>()
 
 template< class T >
-  T
-  fhicl::ParameterSet::get( std::string const & key ) const
+T
+fhicl::ParameterSet::get(std::string const & key) const
 {
-  T  result;
+  T result;
   return get_if_present(key, result) ? result
-                                     : throw fhicl::exception(cant_find, key);
+         : throw fhicl::exception(cant_find, key);
 }
 
 template< class T, class Via >
-  T
-  fhicl::ParameterSet::get( std::string const & key
-                          , T convert(Via const &)
-                          ) const
+T
+fhicl::ParameterSet::get(std::string const & key
+                         , T convert(Via const &)
+                        ) const
 {
-  T  result;
+  T result;
   return get_if_present(key, result, convert)
-       ? result
-       : throw fhicl::exception(cant_find, key);
+         ? result
+         : throw fhicl::exception(cant_find, key);
 }
 
 template< class T >
-  T
-  fhicl::ParameterSet::get( std::string const & key
-                          , T           const & default_value
-                          ) const {
-  T  result;
+T
+fhicl::ParameterSet::get(std::string const & key
+                         , T const & default_value
+                        ) const
+{
+  T result;
   return get_if_present(key, result) ? result
-                                     : default_value;
+         : default_value;
 }
 
 template< class T, class Via >
-  T
-  fhicl::ParameterSet::get( std::string const & key
-                          , T           const & default_value
-                          , T convert(Via const &)
-                          ) const {
-  T  result;
+T
+fhicl::ParameterSet::get(std::string const & key
+                         , T const & default_value
+                         , T convert(Via const &)
+                        ) const
+{
+  T result;
   return get_if_present(key, result, convert)
-       ? result
-       : default_value;
+         ? result
+         : default_value;
 }
 
 // ----------------------------------------------------------------------
 
-inline  bool
-  fhicl::ParameterSet::operator == ( ParameterSet const & other ) const
+inline bool
+fhicl::ParameterSet::operator == (ParameterSet const & other) const
 {
   return id() == other.id();
 }
 
-inline  bool
-  fhicl::ParameterSet::operator != ( ParameterSet const & other ) const
+inline bool
+fhicl::ParameterSet::operator != (ParameterSet const & other) const
 {
   return ! operator==(other);
 }
@@ -290,25 +288,24 @@ inline  bool
 // ----------------------------------------------------------------------
 
 template< class T >
-  bool
-  fhicl::ParameterSet::get_one_( std::string const & key
-                              , T                 & value
-                              ) const
+bool
+fhicl::ParameterSet::get_one_(std::string const & key
+                              , T & value
+                             ) const
 try
 {
   map_iter_t it = mapping_.find(key);
-  if( it == mapping_.end() )
-    return false;
-
+  if (it == mapping_.end())
+  { return false; }
   using detail::decode;
   decode(it->second, value);
   return true;
 }
-catch( fhicl::exception const & e )
+catch (fhicl::exception const & e)
 {
   throw fhicl::exception(type_mismatch, key, e);
 }
-catch( std::exception const & e )
+catch (std::exception const & e)
 {
   throw fhicl::exception(type_mismatch, key + "\n" + e.what());
 }

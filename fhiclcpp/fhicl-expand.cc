e539c2e9d6319cedfacfc6d309600c69a4c32d4e
Kyle Knoepfel <knoepfel@fnal.gov>
2015-08-17 16:29:15 -0500

    Some syntactic cleanup

--- fhiclcpp/fhicl-expand.cc
+++ fhiclcpp/fhicl-expand.cc
@@ -16,59 +16,50 @@
 //  ======================================================================
 
 #include "boost/program_options.hpp"
+#include "cetlib/container_algorithms.h"
 #include "cetlib/exception.h"
 #include "cetlib/filepath_maker.h"
 #include "cetlib/includer.h"
-#include "cpp0x/memory"
-#include "cpp0x/string"
+
 #include <fstream>
 #include <iostream>
 #include <istream>
+#include <memory>
+#include <string>
 #include <vector>
 
-// ----------------------------------------------------------------------
-
-static  std::string const &
-  fhicl_env_var( )
-{
-  static  std::string const  fhicl_env_var("FHICL_FILE_PATH");
-  return fhicl_env_var;
+namespace {
+  std::string const fhicl_env_var{"FHICL_FILE_PATH"};
 }
 
 // ----------------------------------------------------------------------
 
-int
-  do_including( std::string const   & starting_filename
-              , cet::filepath_maker & lookup_policy
-              , std::ostream        & to
-              , std::ostream        & err
-              )
-try
-{
-  cet::includer inc(starting_filename, lookup_policy);
-  cet::includer::const_iterator it  = inc.begin()
-                              , end = inc.end();
-  std::ostream_iterator<char> out(to);
-
-  std::copy(it, end, out);
-  return 0;
-}
-catch( cet::exception const & e )
-{
-  err << "Error for file: " << starting_filename
-      << '\n' << e.what() << '\n';
-  return 1;
-}  // do_including()
+int do_including( std::string const   & starting_filename,
+                  cet::filepath_maker & lookup_policy,
+                  std::ostream        & to,
+                  std::ostream        & err )
+  try
+    {
+      cet::includer inc{starting_filename, lookup_policy};
+      std::ostream_iterator<char> out{to};
+      cet::copy_all(inc, out);
+      return 0;
+    }
+  catch(cet::exception const & e)
+    {
+      err << "Error for file: " << starting_filename
+          << '\n' << e.what() << '\n';
+      return 1;
+    }  // do_including()
 
 // ----------------------------------------------------------------------
 
-int
-  main( int argc, char* argv[] )
+int main( int argc, char* argv[] )
 {
   using namespace std;
   namespace bpo = boost::program_options;
 
-  typedef vector<string> strings;
+  using strings = vector<string>;
 
   // Parse the command line:
 
@@ -80,30 +71,20 @@ int
   string  lookup_path;
 
   desc.add_options()
-    ( "help,h"  , "produce help message"
-                )
-    ( "inputs,i", bpo::value<strings>(&input_filenames)
-                , "input files"
-                )
-    ( "output,o", bpo::value<string >(&output_filename)
-                , "output file"
-                )
-    ( "error,e" , bpo::value<string >(&error_filename)
-                , "error file"
-                )
-    ( "lookup-policy,l"
-                , bpo::value<int>(&lookup_policy)
-                  ->default_value(1)
-                , "lookup policy code:"
-                  "\n  0 => cet::filepath_maker"
-                  "\n  1 => cet::filepath_lookup"
-                  "\n  2 => cet::filepath_lookup_nonabsolute"
-                  "\n  3 => cet::filepath_lookup_after1"
-                )
-    ( "path,p"  , bpo::value<string>(&lookup_path)
-                  ->default_value(fhicl_env_var())
-                , "path or environment variable to be used by lookup-policy"
-                )
+    ( "help,h"  , "produce help message" )
+    ( "inputs,i",  bpo::value<strings>(&input_filenames), "input files")
+    ( "output,o", bpo::value<string >(&output_filename) , "output file")
+    ( "error,e" , bpo::value<string >(&error_filename)  , "error file" )
+    ( "lookup-policy,l" ,
+      bpo::value<int>(&lookup_policy)->default_value(1),
+      "lookup policy code:"
+      "\n  0 => cet::filepath_maker"
+      "\n  1 => cet::filepath_lookup"
+      "\n  2 => cet::filepath_lookup_nonabsolute"
+      "\n  3 => cet::filepath_lookup_after1")
+    ( "path,p"  ,
+      bpo::value<string>(&lookup_path)->default_value(fhicl_env_var),
+      "path or environment variable to be used by lookup-policy")
     ;
 
   bpo::positional_options_description pd;
@@ -111,11 +92,8 @@ int
 
   bpo::variables_map varmap;
   try {
-    bpo::store( bpo::command_line_parser(argc, argv).options(desc)
-                                                    .positional(pd)
-                                                    .run()
-              , varmap
-              );
+    bpo::store( bpo::command_line_parser(argc, argv).
+                options(desc).positional(pd).run(), varmap );
     bpo::notify(varmap);
   }
   catch (bpo::error& err) {
@@ -142,25 +120,25 @@ int
 
   std::unique_ptr<cet::filepath_maker> policy;
 
-cerr << "Policy is " << lookup_policy
-     << "; path is \"" << lookup_path << "\"\n";
+  cerr << "Policy is " << lookup_policy
+       << "; path is \"" << lookup_path << "\"\n";
   switch( lookup_policy ) {
-    case 0:   policy.reset( new cet::filepath_maker );
-              break;
-    case 1:   policy.reset( new cet::filepath_lookup(lookup_path) );
-              break;
-    case 2:   policy.reset( new cet::filepath_lookup_nonabsolute(lookup_path) );
-              break;
-    case 3:   policy.reset( new cet::filepath_lookup_after1(lookup_path) );
-              break;
-    default:  cerr << "Error: command line lookup-policy "
-              << lookup_policy << " is unknown; choose 0, 1, 2, or 3\n";
-              return 1;
+  case 0:   policy = std::make_unique<cet::filepath_maker>();
+    break;
+  case 1:   policy = std::make_unique<cet::filepath_lookup>(lookup_path);
+    break;
+  case 2:   policy = std::make_unique<cet::filepath_lookup_nonabsolute>(lookup_path);
+    break;
+  case 3:   policy = std::make_unique<cet::filepath_lookup_after1>(lookup_path);
+    break;
+  default:  cerr << "Error: command line lookup-policy "
+                 << lookup_policy << " is unknown; choose 0, 1, 2, or 3\n";
+    return 1;
   }
 
   // Do the real work:
 
-  int nfailures = 0;
+  int nfailures{};
   for( size_t k = 0; k != input_filenames.size(); ++k )
     nfailures += do_including(input_filenames[k], *policy, out, err);
   return nfailures;

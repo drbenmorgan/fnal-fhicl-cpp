6e077b9a23c25d5012ad7406c7a55254285d4351
Kyle Knoepfel <knoepfel@fnal.gov>
2015-08-26 08:22:37 -0500

    Adjust deprecation message

--- fhiclcpp/detail/deprecation_msgs.h
+++ fhiclcpp/detail/deprecation_msgs.h
@@ -3,12 +3,12 @@
 
 #define GET_KEYS_MSG                                                    \
   "\n\n"                                                                \
-  "fhiclcpp error: The 'fhicl::ParameterSet::get_keys()' function is deprecated.\n" \
-  "                Please use 'fhicl::ParameterSet::get_names()'.\n\n"
+  "fhiclcpp warning: The 'fhicl::ParameterSet::get_keys()' function is deprecated.\n" \
+  "                  Please use 'fhicl::ParameterSet::get_names()'.\n\n"
 
 #define GET_PSET_KEYS_MSG                                               \
   "\n\n"                                                                \
-  "fhiclcpp error: The 'fhicl::ParameterSet::get_pset_keys()' function is deprecated.\n" \
-  "                Please use 'fhicl::ParameterSet::get_pset_names()'.\n\n"
+  "fhiclcpp warning: The 'fhicl::ParameterSet::get_pset_keys()' function is deprecated.\n" \
+  "                  Please use 'fhicl::ParameterSet::get_pset_names()'.\n\n"
 
 #endif

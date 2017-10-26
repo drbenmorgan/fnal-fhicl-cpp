1f26a22607182046d8da0e839269f86adf4dc0ef
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-10-26 21:46:25 +0100

    Do not link FHiCL python module to libpython
    Python bindings should not directly link to libpython, for
    reasons outlined here (note that it's not macOS specific)
    
    http://blog.tim-smith.us/2015/09/python-extension-modules-os-x/
    
    Change fhicl binding to be a MODULE type by default to give correct
    linking flags. Remove explicit link to libpython so that module
    can be used with all binary compatible interpreters. Use
    dynamic symbol lookup on macOS.
    
    NB: Could also use SHARED, but default CET policy is to require
    full symbol resolution on dynamic libs. That is done via setting
    a default value of CMAKE_SHARED_LINKER_FLAGS, so would have
    to reset/append/prepend to this. MODULE is easy, more correct for
    a Python binding, and should give correct behaviour on all platforms.

--- python/altCMakeLists.cmake
+++ python/altCMakeLists.cmake
@@ -1,23 +1,31 @@
 # - Build Python wrapping for fhiclcpp
 # This is the absolute minimum, so note the following:
-# 1) Should the library type be MODULE?
-# 2) Should the module link to libpython?
-# 3) Where to install (CBT installs alongside the main lib)
+# 1) Where to install (CBT installs alongside the main lib)
 #    Should site-packages be used? But don't want to install
 #    outside CMAKE_INSTALL_PREFIX!
-# 4) See also the comments in the top level altCMakeLists.cmake file
+# 2) See also the comments in the top level altCMakeLists.cmake file
 #    on matching Python/libpython (that's mostly for testing purposes).
-add_library(fhicl SHARED fhiclmodule.cc)
-target_include_directories(fhicl PUBLIC ${PYTHON_INCLUDE_DIRS})
-target_link_libraries(fhicl fhiclcpp ${PYTHON_LIBRARIES})
+# Use MODULE type because this is a true plugin
+# Also, because of CET no-undefined symbol policy for shared libs, have an
+# otherwise awkward workaround of setting/unsetting CMAKE_SHARED_LINKER_FLAGS.
+add_library(fhicl MODULE fhiclmodule.cc)
+target_include_directories(fhicl PRIVATE ${PYTHON_INCLUDE_DIRS})
+# Don't link to libpython, see for why (also applies to linux):
+# http://blog.tim-smith.us/2015/09/python-extension-modules-os-x/
+# ... but make sure we use dynamic lookup for symbols on macOS
+target_link_libraries(fhicl PRIVATE fhiclcpp)
+target_link_libraries(fhicl PRIVATE "$<$<PLATFORM_ID:Darwin>:-undefined dynamic_lookup>")
 
 # Change {pre,suf}fix of module so it's importable (UNIX only)
-set_target_properties(fhicl
-  PROPERTIES
-    PREFIX ""
-    SUFFIX ".so"
-    )
+set_target_properties(fhicl PROPERTIES PREFIX "" SUFFIX ".so")
 
 # Match UPS install, and install module alongside C++ library
+# Ideally, should derive a suitable "site-packages" path from compiled against
+# Python version, e.g. "lib/pythonX.Y/(site|dist)-packages" Should be under install
+# prefix for relocatability.
+# Some Homebrew, but reasonably general, advice here:
+# https://docs.brew.sh/Python-for-Formula-Authors.html
+# and also here for a calling-python method
+# https://stackoverflow.com/questions/1242904/finding-python-site-packages-directory-with-cmake
 install(TARGETS fhicl DESTINATION ${CMAKE_INSTALL_LIBDIR})
 

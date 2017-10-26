6589273c33bdfc7d70bcaa8fd6545f43ae8b29a5
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-10-26 11:40:00 +0100

    Install fhicl Python module
    Temporarily follow UPS procedure of installing Python module alongside
    main fhiclcpp library.
    
    TODO: As with application, review rpath setting to ensure the module
    uses the main library.

--- python/altCMakeLists.cmake
+++ python/altCMakeLists.cmake
@@ -3,10 +3,10 @@
 # 1) Should the library type be MODULE?
 # 2) Should the module link to libpython?
 # 3) Where to install (CBT installs alongside the main lib)
+#    Should site-packages be used? But don't want to install
+#    outside CMAKE_INSTALL_PREFIX!
 # 4) See also the comments in the top level altCMakeLists.cmake file
 #    on matching Python/libpython (that's mostly for testing purposes).
-# 5) Could this be implemented as a setup.py system with CMake
-#    as driver?
 add_library(fhicl SHARED fhiclmodule.cc)
 target_include_directories(fhicl PUBLIC ${PYTHON_INCLUDE_DIRS})
 target_link_libraries(fhicl fhiclcpp ${PYTHON_LIBRARIES})
@@ -17,4 +17,7 @@ set_target_properties(fhicl
     PREFIX ""
     SUFFIX ".so"
     )
+
+# Match UPS install, and install module alongside C++ library
+install(TARGETS fhicl DESTINATION ${CMAKE_INSTALL_LIBDIR})
 
